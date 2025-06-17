180c9ff9f27d5e98e441cdcd515b96a6d7fc1124
ishmaelCEO <irvanterpool@gmail.com>
2025-06-17 02:19:32 +0200

    Test: simplified App.js to debug render

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Booking from "./pages/Booking";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/booking" element={<Booking />} />
-      </Routes>
-    </Router>
+    <div style={{ color: 'gold', backgroundColor: 'black', height: '100vh', padding: '2rem' }}>
+      <h1>✅ App is Rendering!</h1>
+    </div>
   );
 }
 
 export default App;
+
